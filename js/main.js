b5844a33cd33c6120c2e94010a9411a4ac5c0619
DizMaxWork <solskype01@gmail.com>
2022-12-05 19:33:53 +0300

    Предварительный итог

--- js/main.js
+++ js/main.js
@@ -10,45 +10,30 @@ $(document).ready(function (){
         $(".bars").show();
     });
 
-    $(".slick").slick({
-        slidesToShow:10,
-        infinite: false,
-        focusOnSelect: true,
-        nextArrow: "<div class='arrow-2'></div>",
-        prevArrow: "<div class='arrow-1'></div>",
+    $('.center').slick({
+        centerPadding: '60px',
+        slidesToShow: 2,
         responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow:8,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow:6,
-                } 
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow:4,
-                } 
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow:3,
-                } 
-            },
-            {
-                breakpoint: 375,
-                settings: {
-                    slidesToShow:2,
-                } 
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
             }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
         ]
-    })
+      });
 
 
 });
